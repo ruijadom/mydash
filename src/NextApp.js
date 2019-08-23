95cb02334af29f03c8feb90ad081c3336975af4f
ruijadom <ruijadomingues@gmail.com>
2019-08-23 10:40:38 +0100

    App container

---
+++ src/NextApp.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import App from "./containers/App/index";
+
+const NextApp = () => <App />;
+
+export default NextApp;
